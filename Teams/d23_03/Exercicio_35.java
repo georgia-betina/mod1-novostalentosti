e317ed539cd33da303fd409ffa6ad9343a05efee
georgia-betina <georgia_betina@hotmail.com>
2022-03-23 15:07:39 -0300

    Completed Created
    Ex35 completo
    Ex36 criado

--- Teams/d23_03/Exercicio_35.java
+++ Teams/d23_03/Exercicio_35.java
@@ -14,14 +14,17 @@ public class Exercicio_35 {
         System.out.printf("Informe o percentual de impostos: ");
         final int percentualImpostos = leitor.nextInt();
 
-        double aplicaLucro = percentualLucro / 100, aplicaImpostos = percentualImpostos / 100, valorFinal;
+        final int porcentagem = 100;
+        double aplicaLucro = percentualLucro / (float)porcentagem;
+        double aplicaImpostos = percentualImpostos / (float)porcentagem;
+        double valorFinal;
 
         aplicaLucro = valorFabrica * aplicaLucro;
         aplicaImpostos = valorFabrica * aplicaImpostos;
 
         valorFinal = valorFabrica + aplicaImpostos + aplicaLucro;
 
-        System.out.printf("O valor correspondente ao lucro é R$ %.2f%nO valor correspondente aos impostos é R$ %.2f%nO valor final do carro é R$ %.2f", aplicaLucro, aplicaImpostos, valorFinal);
+        System.out.printf("%nO valor correspondente ao lucro é R$ %.2f%nO valor correspondente aos impostos é R$ %.2f%nO valor final do carro é R$ %.2f", aplicaLucro, aplicaImpostos, valorFinal);
 
         leitor.close();
     }
