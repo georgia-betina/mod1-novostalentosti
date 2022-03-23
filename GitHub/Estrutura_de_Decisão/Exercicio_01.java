d130362234a2c0c268ae7afca394d549db4e5f45
georgia-betina <georgia_betina@hotmail.com>
2022-03-23 14:15:12 -0300

    Renomeei arquivos

--- GitHub/Estrutura_de_Decisão/Exercicio_01.java
+++ GitHub/Estrutura_de_Decisão/Exercicio_01.java
@@ -2,7 +2,7 @@ package exercícios.GitHub.Estrutura_de_Decisão;
 
 import java.util.Scanner;
 
-public class Exercício_01 {
+public class Exercicio_01 {
     public static void main(String[] args) {
         System.out.println("Digite um número:");
         Scanner leitor = new Scanner(System.in);
