aad739e3891f6768413bd60e4e6182524f24c789
georgia-betina <georgia_betina@hotmail.com>
2022-03-24 16:50:12 -0300

    Adição de packages

---
+++ GitHub/Estrutura_de_Repetição/Exercicio_02.java
@@ -0,0 +1,17 @@
+package GitHub.Estrutura_de_Repetição;
+
+import java.util.Scanner;
+
+public class Exercicio_02 {
+    public static void main(String[] args) {
+        System.out.printf("Digite um número inteiro: ");
+        Scanner leitor = new Scanner(System.in);
+        //int numeroEntrada = leitor.nextInt();
+
+        //int numeroSaida = 1;
+
+        //while () {}
+
+        leitor.close();
+    }
+}
