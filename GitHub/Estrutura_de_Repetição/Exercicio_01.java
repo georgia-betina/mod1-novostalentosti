06c6872902fff7b8c95a515a4638428cc0d12c5c
georgia-betina <georgia_betina@hotmail.com>
2022-03-24 16:45:00 -0300

    Criação do Ex02

--- GitHub/Estrutura_de_Repetição/Exercicio_01.java
+++ GitHub/Estrutura_de_Repetição/Exercicio_01.java
@@ -1,5 +1,9 @@
 public class Exercicio_01 {
     public static void main(String[] args) {
-        
+        int numero = 0;
+        while (numero <= 100) {
+            numero = numero+1;
+            System.out.println(numero);
+        }
     }
 }
