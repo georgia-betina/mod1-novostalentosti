bdd14eb784abd4628349e4f20e39cef0fd104f06
Georgia Betina <georgia_betina@hotmail.com>
2022-03-28 14:05:53 -0300

    Ex40 completed

--- Teams/d28_03/Exercicio_39.java
+++ Teams/d28_03/Exercicio_39.java
@@ -4,18 +4,21 @@ import java.util.Scanner;
 
 public class Exercicio_39 {
     public static void main(String[] args) {
-        System.out.printf("Escreva o primeiro número:");
+        System.out.printf("Escreva o primeiro número: ");
         Scanner leitor = new Scanner(System.in);
         final float numero1 = leitor.nextFloat();
 
-        System.out.printf("Escreva o segundo número:");
+        System.out.printf("Escreva o segundo número: ");
         final float numero2 = leitor.nextFloat();
 
         if (numero1 == numero2) {
             final double media = (numero1 + numero2) / 2;
             System.out.printf("Os números são iguais e a média entre eles é %.0f.", media);
         } else if (numero1 != numero2) {
-            
+            final double maiorNumero = Math.max(numero1, numero2);
+            final double menorNumero = Math.min(numero1, numero2);
+
+            System.out.printf("Os números são diferentes. %.0f é o maior número e %.0f é o menor.", maiorNumero, menorNumero);
         }
         leitor.close();
     }
---
+++ Teams/d28_03/Exercicio_40.java
@@ -0,0 +1,28 @@
+package Teams.d28_03;
+
+import java.util.Scanner;
+
+public class Exercicio_40 {
+    public static void main(String[] args) {
+        /* Escreva um algoritmo que solicite a quantidade total em miligramas
+        de colesterol mau existente no sangue de um paciente. Considerando que
+        o valor máximo ideal para uma pessoa saudável é 130mg, caso a quantidade
+        esteja menor apresente uma mensagem indicando que esta menor. Caso esteja
+        acima, calcule o percentual que esta acima e apresente uma mensagem. */
+
+        System.out.printf("Informe a quantidade total de colesterol em mg: ");
+        Scanner leitor = new Scanner(System.in);
+        final float qtdeColesterol = leitor.nextFloat();
+        leitor.close();
+
+        final int valorIdeal = 130;
+
+        if (qtdeColesterol <= valorIdeal) {
+            System.out.printf("A quantidade de colesterol presente no sangue está ideal.");
+        } else if (qtdeColesterol > valorIdeal) {
+            final int porCento = 100;
+            final double percentualAcima = ((qtdeColesterol * (float)porCento) / (float)valorIdeal) - porCento;
+            System.out.printf("Você possui %.0f%% de colesterol acima do valor ideal no sangue.", percentualAcima);
+        }
+    }
+}
