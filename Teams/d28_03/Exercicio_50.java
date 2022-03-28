b8db169a4c608ef7d1777a168574f593e415b2ed
Georgia Betina <georgia_betina@hotmail.com>
2022-03-28 16:43:38 -0300

    git fluxograma

---
+++ Teams/d28_03/Exercicio_50.java
@@ -0,0 +1,12 @@
+package Teams.d28_03;
+
+public class Exercicio_50 {
+    public static void main(String[] args) {
+        /* Em uma eleição sindical concorreram ao cargo de presidente
+        três candidatos (A, B e C). Escreva um algoritmo em FLUXOGRAMA
+        que solicite a quantidade de votos de cada candidato, a quantidade
+        de votos nulos e votos em branco. Calcule o percentual DE CADA CANDIDATO
+        em relação ao total de votos válidos. E o percentual de votos nulos e
+        votos em branco do total geral de votos. */        
+    }
+}
