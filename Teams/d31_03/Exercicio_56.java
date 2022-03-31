1af7a5423b42565b925812edf0809cd59c244d81
Geórgia Betina <georgia_betina@hotmail.com>
2022-03-31 14:24:48 -0300

    56 completo

--- Teams/d31_03/Exercicio_56.java
+++ Teams/d31_03/Exercicio_56.java
@@ -2,6 +2,16 @@ package Teams.d31_03;
 
 public class Exercicio_56 {
     public static void main(String[] args) {
-        
+        /* 56) Elabore um programa que apresente no final o somatório dos valores pares existentes na faixa de 1 até 500. */
+        int numero = 1;
+        int soma = 0;
+        while (numero <= 500) {
+            if (numero % 2 == 0) {
+                soma = soma + numero;
+            }
+            numero++;
+        }
+
+        System.out.printf("A soma dos valores pares existentes entre 1 a 500 é %d.", soma);
     }
 }
