3bacc0d26a9ebfa8328dbb2c1217732b1b7b538f
Geórgia Betina <georgia_betina@hotmail.com>
2022-03-31 17:09:10 -0300

    ex 58 criado

---
+++ Teams/d31_03/Exercicio_58.java
@@ -0,0 +1,10 @@
+package Teams.d31_03;
+
+public class Exercicio_58 {
+    public static void main(String[] args) {
+        /* 58) Faça um programa que leia um número N que
+        indica quantos valores inteiros e positivos devem
+        ser lidos a seguir. Para cada número lido, mostre
+        o valor lido e o fatorial desse valor. */
+    }
+}
