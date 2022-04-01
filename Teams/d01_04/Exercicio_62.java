c8aa8274b6ec14d38ba6bca1168b069b0844ef4d
georgia betina <georgia_betina@hotmail.com>
2022-04-01 16:44:28 -0300

    EX 62 criado

---
+++ Teams/d01_04/Exercicio_62.java
@@ -0,0 +1,12 @@
+package Teams.d01_04;
+
+public class Exercicio_62 {
+    public static void main(String[] args) {
+        /* 62) Elaborar um programa que efetue a leitura de
+        valores positivos inteiros até que um valor negativo
+        seja informado. Ao final deve ser apresentados o maior
+        e o menor número informado pelo usuário. */
+
+        
+    }
+}
