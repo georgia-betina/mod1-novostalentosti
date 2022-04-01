d6bb314f3083934d704071b4e27aad0d53a3ecb4
georgia betina <georgia_betina@hotmail.com>
2022-04-01 16:58:07 -0300

    ex 62 completo

--- Teams/d01_04/Exercicio_62.java
+++ Teams/d01_04/Exercicio_62.java
@@ -1,5 +1,7 @@
 package Teams.d01_04;
 
+import java.util.Scanner;
+
 public class Exercicio_62 {
     public static void main(String[] args) {
         /* 62) Elaborar um programa que efetue a leitura de
@@ -7,6 +9,22 @@ public class Exercicio_62 {
         seja informado. Ao final deve ser apresentados o maior
         e o menor número informado pelo usuário. */
 
-        
+        int entradaNumero = 0;
+        int maiorEntrada = 0;
+        int menorEntrada;
+
+        Scanner leitor = new Scanner(System.in);
+        do {
+            System.out.printf("Digite um número inteiro e positivo: ");
+            menorEntrada = entradaNumero;
+            entradaNumero = leitor.nextInt();
+            if (entradaNumero > 0) {
+                maiorEntrada = Math.max(entradaNumero, maiorEntrada);
+                menorEntrada = Math.min(entradaNumero, menorEntrada);
+            }
+        } while (entradaNumero > 0);
+        leitor.close();
+
+        System.out.println("O maior número apresentado foi " + maiorEntrada + " e o menor número apresentado foi " + menorEntrada);
     }
 }
