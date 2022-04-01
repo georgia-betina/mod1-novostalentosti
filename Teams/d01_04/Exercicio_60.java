8b44d6cab4b02ab3ed41a2ea430278277dc62314
georgia betina <georgia_betina@hotmail.com>
2022-04-01 16:17:53 -0300

    ex 60 criado

---
+++ Teams/d01_04/Exercicio_60.java
@@ -0,0 +1,11 @@
+package Teams.d01_04;
+
+public class Exercicio_60 {
+    public static void main(String[] args) {
+        /* 60) Elaborar um programa que efetue a leitura
+        de 10 valores numéricos e apresente no final o
+        total do somatório e a média do total. */
+
+        
+    }
+}
