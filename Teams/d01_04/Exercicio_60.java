1c2f52fdce0432ad7008104823e13d64fb22a6f3
georgia betina <georgia_betina@hotmail.com>
2022-04-01 16:29:24 -0300

    ex60 completo|revisar

--- Teams/d01_04/Exercicio_60.java
+++ Teams/d01_04/Exercicio_60.java
@@ -1,11 +1,26 @@
 package Teams.d01_04;
 
+import java.util.Scanner;
+
 public class Exercicio_60 {
     public static void main(String[] args) {
         /* 60) Elaborar um programa que efetue a leitura
         de 10 valores numéricos e apresente no final o
         total do somatório e a média do total. */
 
-        
+        int contaAte10;
+        double media = 0;
+        Scanner leitor = new Scanner(System.in);
+        for (contaAte10 = 1; contaAte10 <= 10; contaAte10++) {
+            System.out.printf("Informe o valor do número %d: ", contaAte10);
+            float numero = leitor.nextFloat();
+            media += numero;
+        }
+        leitor.close();
+
+        contaAte10--;
+        media = media / contaAte10;
+
+        System.out.printf("A média das notas é: %.1f", media);
     }
 }
