70c9ae54c88bb60fb2d4215f2ff0584610551c25
georgia betina <georgia_betina@hotmail.com>
2022-04-01 16:43:45 -0300

    EX 61 completo

--- Teams/d01_04/Exercicio_61.java
+++ Teams/d01_04/Exercicio_61.java
@@ -1,5 +1,7 @@
 package Teams.d01_04;
 
+import java.util.Scanner;
+
 public class Exercicio_61 {
     public static void main(String[] args) {
         /* 61) Elaborar um programa que efetue a leitura
@@ -10,6 +12,29 @@ public class Exercicio_61 {
         Ou seja, o programa deve parar quando o usuário fornecer
         um valor negativo. */
 
+        Scanner leitor = new Scanner(System.in);
+
+        
+        int contador = 0;
+        double entradaNumero = 0;
+        double soma = 0;
+        double media = 0;
+
+        do {
+            System.out.printf("Informe um número positivo: ");
+            entradaNumero = leitor.nextDouble();
+            if (entradaNumero > 0) {
+                soma += entradaNumero;
+            }
+            contador++;
+        } while (entradaNumero > 0);
+
+        leitor.close();
+
+        contador--;
+        media = soma / contador;
+
+        System.out.printf("A quantidade de números digitados é: %d%nA soma de todos os números é %.2f%nA média de todos os números é %.2f", contador, soma, media);
         
     }
 }
