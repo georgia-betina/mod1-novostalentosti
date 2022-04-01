c8e3abb76c1c7fc9b35ccd973129bbd63ba7f644
georgia betina <georgia_betina@hotmail.com>
2022-04-01 16:07:14 -0300

    enunciado

--- Teams/d01_04/Exercicio_59.java
+++ Teams/d01_04/Exercicio_59.java
@@ -2,6 +2,12 @@ package Teams.d01_04;
 
 public class Exercicio_59 {
     public static void main(String[] args) {
+        /* 59) Elaborar um programa que apresente os valores
+        de conversão de graus Celsius em Fahrenheit, de 10 em
+        10 graus, iniciando a contagem em 10 graus Celsius e
+        finalizando em 100 graus Celsius. O programa deverá
+        apresentar os valores das duas temperaturas. */
+
         
     }
 }
