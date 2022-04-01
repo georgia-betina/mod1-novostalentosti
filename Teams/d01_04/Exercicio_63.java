b791c1324939816fd80bd788acba29cc83aa69a5
georgia betina <georgia_betina@hotmail.com>
2022-04-01 16:59:41 -0300

    ex 63 criado

---
+++ Teams/d01_04/Exercicio_63.java
@@ -0,0 +1,17 @@
+package Teams.d01_04;
+
+public class Exercicio_63 {
+    public static void main(String[] args) {
+        /* 63) Faça um programa que receba 'vários números, calcule e mostre:
+        - a soma dos números digitados;
+        - a quantidade de números digitados;
+        - a média dos números digitados;
+        - o maior número digitado;
+        - o menor número digitado;
+        - a média dos números pares;
+        - a porcentagem dos números ímpares entre todos os números digitados.
+        Finalize a entrada de dados com a digitação do número 30000. */
+
+        
+    }
+}
