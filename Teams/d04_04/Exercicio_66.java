76a0a80ffa67600926a511065013f8285e194b80
Geórgia Betina <georgia_betina@hotmail.com>
2022-04-04 14:24:45 -0300

    ex66 criado

---
+++ Teams/d04_04/Exercicio_66.java
@@ -0,0 +1,13 @@
+package Teams.d04_04;
+
+public class Exercicio_66 {
+    public static void main(String[] args) {
+        /* 66) Faça um algoritmo que calcule a soma de
+        todos os números ímpares dentro de uma faixa de
+        valores determinada pelo usuário. Um número é ímpar
+        quando sua divisão por 2 não é exata, ou seja, o resto
+        resultante da divisão inteira pelo número 2 tem valor 1.
+        Utilize a palavra resto como operador que calcula o resto
+        da divisão de um numero por outro. */
+    }
+}
