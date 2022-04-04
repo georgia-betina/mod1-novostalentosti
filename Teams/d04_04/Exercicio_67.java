6e1fe8fb0f4d9705d3de9690980522d6f4247d3c
Geórgia Betina <georgia_betina@hotmail.com>
2022-04-04 14:58:22 -0300

    ex67 criado

---
+++ Teams/d04_04/Exercicio_67.java
@@ -0,0 +1,18 @@
+package Teams.d04_04;
+
+public class Exercicio_67 {
+    public static void main(String[] args) {
+        /* 67) Faça um programa que apresente os resultados
+        de uma tabuada de um número qualquer, a qual deve ser
+        impressa no seguinte formato:
+        Considerando como exemplo o fornecimento do número 2
+        para o número qualquer, ter-se-ia a seguinte situação:
+        2 x 1 = 2
+        2 x 2 = 4
+        2 x 3 = 6
+        ...
+        2 x 10 = 20 */
+
+        
+    }
+}
