1e8f2077b6353dafcf460529b4aabd33721837bd
Geórgia Betina <georgia_betina@hotmail.com>
2022-04-04 14:23:59 -0300

    ex65 completo

--- Teams/d04_04/Exercicio_65.java
+++ Teams/d04_04/Exercicio_65.java
@@ -1,5 +1,7 @@
 package Teams.d04_04;
 
+import java.util.Scanner;
+
 public class Exercicio_65 {
     public static void main(String[] args) {
         /* 65) Faça um algoritmo que leia um número inteiro
@@ -8,6 +10,29 @@ public class Exercicio_65 {
         de um número n, representado por n!, é dado por:
         n * (n-1) * (n-2) * ... * 1, para n > 0 e n! = 1 para n = 0 */
 
-        
+        float entradaNumero = 0f;
+        Scanner leitor = new Scanner(System.in);
+
+        do {
+            System.out.printf("Digite um inteiro maior ou igual a zero: ");
+            entradaNumero = (float)leitor.nextFloat();
+            int contador = 1;
+            double fatorial = 1;
+            if (entradaNumero >=0 && entradaNumero != 1) {
+                while (contador < entradaNumero) {
+                    for (int i = 1; i <= entradaNumero; i++) {
+                        fatorial *= contador;
+                        contador++;
+                    }
+                }
+                System.out.printf("O fatorial de %.0f é %.0f%n", entradaNumero, fatorial);
+            } else if (entradaNumero < 0) {
+                System.out.printf("Número inválido. ");
+            } else if (entradaNumero == 1) {
+                System.out.printf("O fatorial de %.0f é 0%n", entradaNumero);
+            }
+        } while (entradaNumero >= 0);
+
+        leitor.close();
     }
 }
