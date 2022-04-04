9280e39a78d779cca380cdc91d1ebb3a26540516
Geórgia Betina <georgia_betina@hotmail.com>
2022-04-04 16:56:02 -0300

    EX75 completo

---
+++ Teams/d04_04/Exercicio_75.java
@@ -0,0 +1,25 @@
+package Teams.d04_04;
+
+public class Exercicio_75 {
+    public static void main(String[] args) {
+        /* 75) Faça um programa que receba a idade
+        de 15 pessoas e que calcule e mostre:
+
+        - A quantidade de pessoas em cada faixa etária
+        - A percentagem de pessoas em cada uma das faixas
+        etárias, com relação ao total de pessoas, de
+        acordo com a tabela abaixo:
+        
+        --------------+-----------------------------------
+        Faixa Etária  | Idade
+        --------------+-----------------------------------
+        1             | Até 15 anos
+        2             | De 16 a 30 anos
+        3             | De 31 a 45 anos
+        4             | De 46 a 60 anos
+        5             | Acima de 61 anos
+        --------------+----------------------------------- */
+
+        
+    }
+}
