395360c09db74d3da05b2755d3f0e67934ee0a1d
Geórgia Betina <georgia_betina@hotmail.com>
2022-04-04 13:52:08 -0300

    EX65 criado

---
+++ Teams/d04_04/Exercicio_65.java
@@ -0,0 +1,13 @@
+package Teams.d04_04;
+
+public class Exercicio_65 {
+    public static void main(String[] args) {
+        /* 65) Faça um algoritmo que leia um número inteiro
+        e calcule o seu fatorial. Se o número for negativo,
+        informe que o valor é inválido. Sabemos que o fatorial
+        de um número n, representado por n!, é dado por:
+        n * (n-1) * (n-2) * ... * 1, para n > 0 e n! = 1 para n = 0 */
+
+        
+    }
+}
