8faa4dd16fb0cc2829f3add87ce0977e22c7a7c7
Geórgia Betina <georgia_betina@hotmail.com>
2022-04-04 17:09:30 -0300

    ex75 completo

--- Teams/d04_04/Exercicio_75.java
+++ Teams/d04_04/Exercicio_75.java
@@ -1,5 +1,7 @@
 package Teams.d04_04;
 
+import java.util.Scanner;
+
 public class Exercicio_75 {
     public static void main(String[] args) {
         /* 75) Faça um programa que receba a idade
@@ -20,6 +22,41 @@ public class Exercicio_75 {
         5             | Acima de 61 anos
         --------------+----------------------------------- */
 
-        
+        int idade = 0;
+        int contaFaixaEtaria1 = 0;
+        int contaFaixaEtaria2 = 0;
+        int contaFaixaEtaria3 = 0;
+        int contaFaixaEtaria4 = 0;
+        int contaFaixaEtaria5 = 0;
+
+        Scanner leitor = new Scanner(System.in);
+
+        for (int i = 1; i <=15; i++) {
+            System.out.printf("Pessoa %d, informe a sua idade: ", i);
+            idade = leitor.nextInt();
+
+            if (idade == 0) {
+                System.out.printf("Valor inválido.");
+            } else if (idade <= 15) {
+                contaFaixaEtaria1++;
+            } else if (idade >= 16 && idade <= 30) {
+                contaFaixaEtaria2++;
+            } else if (idade >= 31 && idade <= 45) {
+                contaFaixaEtaria3++;
+            } else if (idade >= 46 && idade <= 60) {
+                contaFaixaEtaria4++;
+            } else if (idade >= 61) {
+                contaFaixaEtaria5++;
+            }
+        }
+        leitor.close();
+
+        float percentualFaixa1 = (contaFaixaEtaria1 * (float)100) / 15;
+        float percentualFaixa2 = (contaFaixaEtaria2 * (float)100) / 15;
+        float percentualFaixa3 = (contaFaixaEtaria3 * (float)100) / 15;
+        float percentualFaixa4 = (contaFaixaEtaria4 * (float)100) / 15;
+        float percentualFaixa5 = (contaFaixaEtaria5 * (float)100) / 15;
+
+        System.out.printf("Existem %d pessoas na faixa etária 1;%n%d pessoas na faixa etária 2;%n%d pessoas na faixa etária 3;%n%d pessoas na faixa etária 4%n%d pessoas na faixa etária 5.%n%nO percentual de pessoas na faixa etária 1 é %.1f%%, na faixa 2 %.1f%%, na faixa 3 %.1f%%, na faixa 4 %.1f%% e na faixa 5 %.1f%%.", contaFaixaEtaria1, contaFaixaEtaria2, contaFaixaEtaria3, contaFaixaEtaria4, contaFaixaEtaria5, percentualFaixa1, percentualFaixa2, percentualFaixa3, percentualFaixa4, percentualFaixa5);
     }
 }
