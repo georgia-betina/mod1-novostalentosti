2691d08bade02df4128e1571bdc9c391600a69bf
georgia betina <georgia_betina@hotmail.com>
2022-04-05 17:06:18 -0300

    ex 5 criado

---
+++ GitHub/Estrutura_de_Repetição/Exercicio_05.java
@@ -0,0 +1,9 @@
+package GitHub.Estrutura_de_Repetição;
+
+public class Exercicio_05 {
+    public static void main(String[] args) {
+        /* Faça um programa para calcular e escrever a soma
+        dos cubos dos números pares compreendidos entre A e B
+        (B > A). A e B são lidos pelo teclado. */
+    }
+}
