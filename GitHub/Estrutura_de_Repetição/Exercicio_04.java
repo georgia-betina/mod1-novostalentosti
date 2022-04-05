a463a36ca808c01ab10d37b1daca504e3110ae7f
georgia betina <georgia_betina@hotmail.com>
2022-04-05 16:49:02 -0300

    ex 4 criado

---
+++ GitHub/Estrutura_de_Repetição/Exercicio_04.java
@@ -0,0 +1,9 @@
+package GitHub.Estrutura_de_Repetição;
+
+public class Exercicio_04 {
+    public static void main(String[] args) {
+        /* Construa um programa que exiba a tabuada de 1 até N,
+        onde N é informado pelo usuário. ex: Até a tabuada de 3,
+        irá imprimir as tabuadas de 1, 2 e 3. */
+    }   
+}
