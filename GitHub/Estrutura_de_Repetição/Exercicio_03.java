92c0fd13fddce65bb2a5c985ae6937da7f706d9d
georgia betina <georgia_betina@hotmail.com>
2022-04-05 16:47:42 -0300

    ex 3 completo

--- GitHub/Estrutura_de_Repetição/Exercicio_03.java
+++ GitHub/Estrutura_de_Repetição/Exercicio_03.java
@@ -1,5 +1,7 @@
 package GitHub.Estrutura_de_Repetição;
 
+import java.util.Scanner;
+
 public class Exercicio_03 {
     public static void main(String[] args) {
         /* Faça um programa que peça dois números, base e
@@ -7,6 +9,22 @@ public class Exercicio_03 {
         elevado ao segundo número. Não utilize a
         função de potência da linguagem. */
 
-        
+        Scanner leitor = new Scanner(System.in);
+        System.out.printf("Informe um número (base): ");
+        final int numeroBase = leitor.nextInt();
+
+        System.out.printf("Informe outro número (expoente): ");
+        final int numeroExpoente = leitor.nextInt();
+
+        leitor.close();
+
+        int potencia = 1;
+
+        for (int i = 1; i <= numeroExpoente; i++) {
+            potencia *= numeroBase;
+        }
+
+        System.out.printf("A potência de %d elevado a %d é %d.", numeroBase, numeroExpoente, potencia);
+
     }
 }
