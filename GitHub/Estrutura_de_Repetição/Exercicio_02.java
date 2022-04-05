b2f1822afe0589149a403ba310034a2b62918c2e
georgia betina <georgia_betina@hotmail.com>
2022-04-05 16:41:15 -0300

    EX02 completo

--- GitHub/Estrutura_de_Repetição/Exercicio_02.java
+++ GitHub/Estrutura_de_Repetição/Exercicio_02.java
@@ -4,14 +4,33 @@ import java.util.Scanner;
 
 public class Exercicio_02 {
     public static void main(String[] args) {
-        System.out.printf("Digite um número inteiro: ");
+        /* Escreva um programa que pergunte ao usuário um número
+        e após, escreva na tela a soma total de 1 até o número
+        lido. Exemplo: 5: 1 + 2 + 3 + 4 + 5 = 15 */
         Scanner leitor = new Scanner(System.in);
-        //int numeroEntrada = leitor.nextInt();
 
-        //int numeroSaida = 1;
+        int entradaNumero = 0;
+        int somaNumeros = 0;
+        do {
+            System.out.printf("Digite um número inteiro e positivo: ");
+            entradaNumero = leitor.nextInt();
 
-        //while () {}
-
+            if (entradaNumero > 0) {
+                for (int i = entradaNumero; i >= 1; i--) {
+                    somaNumeros += i;
+                    if (i != 1) {
+                        System.out.printf("%d + ", i);
+                    } else if (i == 1) {
+                        System.out.printf("%d", i);
+                    }
+                }
+    
+                System.out.printf(" = %d%n", somaNumeros);
+                somaNumeros = 0;
+            } else if (entradaNumero <= 0) {
+                System.out.printf("Valor inválido. Encerrando operação...");
+            }
+        } while (entradaNumero > 0);
         leitor.close();
     }
 }
