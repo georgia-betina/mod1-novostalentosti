2687c4f81d6c1990a42579fd3f55c82f32c67a72
georgia betina <georgia_betina@hotmail.com>
2022-04-05 17:17:11 -0300

    desenvolvimento 5

--- GitHub/Estrutura_de_Repetição/Exercicio_05.java
+++ GitHub/Estrutura_de_Repetição/Exercicio_05.java
@@ -1,9 +1,45 @@
 package GitHub.Estrutura_de_Repetição;
 
+import java.util.Scanner;
+
 public class Exercicio_05 {
     public static void main(String[] args) {
-        /* Faça um programa para calcular e escrever a soma
-        dos cubos dos números pares compreendidos entre A e B
-        (B > A). A e B são lidos pelo teclado. */
+        /*
+         * Faça um programa para calcular e escrever a soma
+         * dos cubos dos números pares compreendidos entre A e B
+         * (B > A). A e B são lidos pelo teclado.
+         */
+
+        Scanner leitor = new Scanner(System.in);
+        int valorA = 0;
+        int valorB = 0;
+
+        do {
+            System.out.printf("Informe o valor de A: ");
+            valorA = leitor.nextInt();
+
+            System.out.printf("Informe o valor de B (sendo que B deve ser maior que A: ");
+            valorB = leitor.nextInt();
+
+            int aumentaContagem = valorA;
+            int aoCubo = aumentaContagem;
+            int somaNumeros = 0;
+
+            if (valorB > valorA) {
+                for (int i = valorA; i <= valorB; i++) {
+                    if (aumentaContagem % 2 == 0) {
+                        aoCubo *= aumentaContagem * aumentaContagem;
+                        somaNumeros += aoCubo;
+                        aumentaContagem++;
+                        aoCubo = aumentaContagem;
+                    }
+                }
+                System.out.printf("A soma é %d", somaNumeros);
+                // contador até valor B -> os resultados sao pares? se sim, calcula o cubo e soma
+            }
+
+        } while (valorB > valorA);
+
+        leitor.close();
     }
 }
