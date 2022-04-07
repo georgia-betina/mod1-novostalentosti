35ceca8f6164557ed369af01f567788049870871
georgia betina <georgia_betina@hotmail.com>
2022-04-07 15:05:11 -0300

    desenvolvimento ex72

--- Teams/d07_04/Exercicio_72.java
+++ Teams/d07_04/Exercicio_72.java
@@ -1,7 +1,27 @@
 package Teams.d07_04;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Exercicio_72 {
     public static void main(String[] args) {
-        
+        /* 72) Faça um programa que receba dez números
+        e armazene em uma lista. Em seguida percorra
+        toda a lista mostrando apenas os números que
+        cujo valor seja superior a 10. */
+
+        Scanner leitor = new Scanner(System.in);
+        int[] listaNumeros = new int[10];
+
+        for (int i = 0; i < listaNumeros.length; i++) {
+            System.out.printf("Informe 10 números: ");
+            listaNumeros[i] = leitor.nextInt();
+            for (int j = 0; j < listaNumeros.length; j++) { // filtro
+                if (listaNumeros[i] > 10) {
+                    System.out.println(listaNumeros[i]);
+                }
+            }
+        }
+        leitor.close();
     }
 }
