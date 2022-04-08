18b3e2610ed3a1d3877c55e48b58fe98892e87ae
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-08 16:15:28 -0300

    ex 80 completo

--- Teams/d08_04/Exercicio_80.java
+++ Teams/d08_04/Exercicio_80.java
@@ -1,11 +1,44 @@
 package Teams.d08_04;
 
+import java.util.Scanner;
+
 public class Exercicio_80 {
     public static void main(String[] args) {
-        /* 80) Faça um programa que carregue dois vetores de dez
-        elementos numéricos cada um e mostre um vetor resultante
-        da intercalação desses dois vetores. */
+        /*
+         * 80) Faça um programa que carregue dois vetores de dez
+         * elementos numéricos cada um e mostre um vetor resultante
+         * da intercalação desses dois vetores.
+         */
 
-        
+        Scanner leitor = new Scanner(System.in);
+        int[] listaNumeros1 = new int[10];
+        int[] listaNumeros2 = new int[10];
+        int[] vetorResultante = new int[20];
+        int contador1 = 0;
+        int contador2 = 0;
+
+        for (int i = 0; i < listaNumeros1.length; i++) {
+            System.out.printf("Informe um número: ");
+            listaNumeros1[i] = leitor.nextInt();
+        }
+
+        for (int i = 0; i < listaNumeros2.length; i++) {
+            System.out.printf("Informe outro número: ");
+            listaNumeros2[i] = leitor.nextInt();
+        }
+
+        for (int i = 0; i < 10; i++) {
+
+            vetorResultante[contador1] = listaNumeros1[contador2];
+            System.out.printf("%d - ", vetorResultante[contador1]);
+            contador1++;
+
+            vetorResultante[contador1] = listaNumeros2[contador2];
+            System.out.printf("%d%n", vetorResultante[contador1]);
+            contador1++;
+            contador2++;
+        }
+
+        leitor.close();
     }
 }
