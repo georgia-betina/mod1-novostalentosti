7a872ab14547480da26befa5752e1b00804a54db
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-08 14:18:02 -0300

    ex 73 criado

---
+++ Teams/d08_04/Exercicio_73.java
@@ -0,0 +1,11 @@
+package Teams.d08_04;
+
+public class Exercicio_73 {
+    public static void main(String[] args) {
+        /* 73) Faça um programa que receba dez números e armazene
+        em uma lista. Em seguida conte quantos números são impar e
+        quantos são par. Apresente os contadores na tela. */
+
+        
+    }
+}
