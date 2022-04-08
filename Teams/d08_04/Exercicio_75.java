7d834ec48ba62a010022a4ebfe856f3be6109ba6
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-08 14:41:29 -0300

    ex 75 completo

--- Teams/d08_04/Exercicio_75.java
+++ Teams/d08_04/Exercicio_75.java
@@ -1,5 +1,7 @@
 package Teams.d08_04;
 
+import java.util.Scanner;
+
 public class Exercicio_75 {
     public static void main(String[] args) {
         /* 75) Faça um programa que receba dez números e armazene em uma
@@ -7,6 +9,26 @@ public class Exercicio_75 {
         novamente a lista. Apresente a soma e em seguida a média baseada
         na soma e no número de números armazenados. */
 
-        
+        Scanner leitor = new Scanner(System.in);
+        int[] listaNumeros = new int[10];
+        int somaNumeros = 0;
+        int contador = 0;
+
+        for (int i = 0; i < listaNumeros.length; i++) {
+            System.out.printf("Informe um número: ");
+            listaNumeros[i] = leitor.nextInt();
+            contador++;
+        }
+
+        leitor.close();
+
+        for (int i = 0; i < listaNumeros.length; i++) {
+            somaNumeros += listaNumeros[i];
+        }
+
+        final float media = (float)somaNumeros / (float)contador;
+
+        System.out.printf("A soma é %d%nA média é %.1f", somaNumeros, media);
+
     }
 }
