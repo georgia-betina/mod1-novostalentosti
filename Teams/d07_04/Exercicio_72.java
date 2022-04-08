27649f80ac31b202e7b4161b6ed566e1fc2efb50
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-08 14:15:27 -0300

    ex72 completo

--- Teams/d07_04/Exercicio_72.java
+++ Teams/d07_04/Exercicio_72.java
@@ -1,27 +1,33 @@
 package Teams.d07_04;
 
-import java.util.Arrays;
+//import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exercicio_72 {
     public static void main(String[] args) {
-        /* 72) Faça um programa que receba dez números
-        e armazene em uma lista. Em seguida percorra
-        toda a lista mostrando apenas os números que
-        cujo valor seja superior a 10. */
+        /*
+         * 72) Faça um programa que receba dez números
+         * e armazene em uma lista. Em seguida percorra
+         * toda a lista mostrando apenas os números que
+         * cujo valor seja superior a 10.
+         */
 
-        Scanner leitor = new Scanner(System.in);
         int[] listaNumeros = new int[10];
 
-        for (int i = 0; i < listaNumeros.length; i++) {
-            System.out.printf("Informe 10 números: ");
-            listaNumeros[i] = leitor.nextInt();
-            for (int j = 0; j < listaNumeros.length; j++) { // filtro
-                if (listaNumeros[i] > 10) {
-                    System.out.println(listaNumeros[i]);
-                }
-            }
+        Scanner leitor = new Scanner(System.in);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Digite um numero");
+            listaNumeros[i] = Integer.parseInt(leitor.nextLine());
         }
+
         leitor.close();
+
+        System.out.printf("Os números maiores que 10 são: ");
+        for (int j = 0; j < 10; j++) {
+            if (listaNumeros[j] > 10) { // filtro
+                System.out.printf("%d ", listaNumeros[j]);
+            } 
+        }
     }
 }
