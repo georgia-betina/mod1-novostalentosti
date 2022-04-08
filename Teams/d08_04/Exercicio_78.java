0573d0c5e0d8dc77cbafff8fd308c2d9365d5592
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-08 15:08:29 -0300

    ex 78 completo

--- Teams/d08_04/Exercicio_78.java
+++ Teams/d08_04/Exercicio_78.java
@@ -1,11 +1,30 @@
 package Teams.d08_04;
 
+import java.util.Scanner;
+
 public class Exercicio_78 {
     public static void main(String[] args) {
         /* 78) Faça um programa que receba dez números e armazene em
         uma lista. Em seguida copie todos os números da primeira lista
         para uma segunda lista, mas na ordem invertida da primeira lista. */
 
-        
+        Scanner leitor = new Scanner(System.in);
+        int[] listaNumeros = new int[10];
+        int[] listaNumerosInvertidos = new int[10];
+
+        for (int i = 0; i < listaNumeros.length; i++) {
+            System.out.printf("Informe um número: ");
+            listaNumeros[i] = leitor.nextInt();
+        }
+
+        leitor.close();
+
+        int contador = 0;
+        System.out.printf("A lista invertida é: ");
+        for (int i = 9; i >= 0; i--) {
+            listaNumerosInvertidos[contador] = listaNumeros[i];
+            System.out.println(listaNumerosInvertidos[contador]);
+            contador++;
+        }
     }
 }
