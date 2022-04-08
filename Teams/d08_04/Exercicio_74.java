5a78bb24cc336baa591a5f833a1241f8e1e156a4
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-08 14:26:54 -0300

    ex 74 criado

---
+++ Teams/d08_04/Exercicio_74.java
@@ -0,0 +1,12 @@
+package Teams.d08_04;
+
+public class Exercicio_74 {
+    public static void main(String[] args) {
+        /* 74) Faça um programa que receba dez números e armazene
+        em uma lista. Em seguida, substitua todos os números cujo
+        valor seja menor que 10 pelo valor ZERO. Imprima a lista
+        em tela. */
+
+        
+    }
+}
