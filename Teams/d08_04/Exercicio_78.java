7e5d1a9dc1040da5ce727ce5eb12e05fc363c256
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-08 14:56:47 -0300

    exercicio 78 criado

---
+++ Teams/d08_04/Exercicio_78.java
@@ -0,0 +1,11 @@
+package Teams.d08_04;
+
+public class Exercicio_78 {
+    public static void main(String[] args) {
+        /* 78) Faça um programa que receba dez números e armazene em
+        uma lista. Em seguida copie todos os números da primeira lista
+        para uma segunda lista, mas na ordem invertida da primeira lista. */
+
+        
+    }
+}
