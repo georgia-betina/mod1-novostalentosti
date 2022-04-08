dedd1850f48c2a119b2a5416d552df44644e8a5a
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-08 14:55:59 -0300

    ex 77 completo

--- Teams/d08_04/Exercicio_77.java
+++ Teams/d08_04/Exercicio_77.java
@@ -1,5 +1,7 @@
 package Teams.d08_04;
 
+import java.util.Scanner;
+
 public class Exercicio_77 {
     public static void main(String[] args) {
         /* 77) Faça um programa que receba dez números e
@@ -7,6 +9,24 @@ public class Exercicio_77 {
         número e armazene em uma variável chamada multiplicador.
         Percorra todo a lista e multiplique cada número pelo
         multiplicador e apresente em tela. */
-        
+
+        Scanner leitor = new Scanner(System.in);
+        int[] listaNumeros = new int[10];
+
+        for (int i = 0; i < listaNumeros.length; i++) {
+            System.out.printf("Informe um número: ");
+            listaNumeros[i] = leitor.nextInt();
+        }
+
+        int multiplicador;
+        System.out.printf("Informe outro número para agir como multiplicador: ");
+        multiplicador = leitor.nextInt();
+
+        leitor.close();
+
+        for (int i = 0; i < listaNumeros.length; i++) {
+            listaNumeros[i] *= multiplicador;
+            System.out.println(listaNumeros[i]);
+        }
     }
 }
