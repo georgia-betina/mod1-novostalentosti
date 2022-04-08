b6db43159fed2106e35ec6fad458bfd8c0516a02
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-08 14:34:03 -0300

    ex 75 criado

---
+++ Teams/d08_04/Exercicio_75.java
@@ -0,0 +1,12 @@
+package Teams.d08_04;
+
+public class Exercicio_75 {
+    public static void main(String[] args) {
+        /* 75) Faça um programa que receba dez números e armazene em uma
+        lista. Em seguida calcule a soma de todos os números percorrendo
+        novamente a lista. Apresente a soma e em seguida a média baseada
+        na soma e no número de números armazenados. */
+
+        
+    }
+}
