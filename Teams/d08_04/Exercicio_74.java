5ed446ffffb64963a7b32adcf84d58b28030bea8
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-08 14:32:10 -0300

    ex 74 completo

--- Teams/d08_04/Exercicio_74.java
+++ Teams/d08_04/Exercicio_74.java
@@ -1,5 +1,7 @@
 package Teams.d08_04;
 
+import java.util.Scanner;
+
 public class Exercicio_74 {
     public static void main(String[] args) {
         /* 74) Faça um programa que receba dez números e armazene
@@ -7,6 +9,21 @@ public class Exercicio_74 {
         valor seja menor que 10 pelo valor ZERO. Imprima a lista
         em tela. */
 
-        
+        Scanner leitor = new Scanner(System.in);
+        int[] listaNumeros = new int[10];
+
+        for (int i = 0; i < listaNumeros.length; i++) {
+            System.out.printf("Informe um número: ");
+            listaNumeros[i] = leitor.nextInt();
+        }
+
+        leitor.close();
+
+        for (int i = 0; i < listaNumeros.length; i++) {
+            if (listaNumeros[i] < 10) {
+                listaNumeros[i] = 0;
+            }
+            System.out.println(listaNumeros[i]);
+        }
     }
 }
