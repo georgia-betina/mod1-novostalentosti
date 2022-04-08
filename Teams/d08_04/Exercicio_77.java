8ad047ec92aeea25acf6ae3799ce8d819ddcf267
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-08 14:49:39 -0300

    ex 77 criado

---
+++ Teams/d08_04/Exercicio_77.java
@@ -0,0 +1,12 @@
+package Teams.d08_04;
+
+public class Exercicio_77 {
+    public static void main(String[] args) {
+        /* 77) Faça um programa que receba dez números e
+        armazene em uma lista. Em seguida solicite um outro
+        número e armazene em uma variável chamada multiplicador.
+        Percorra todo a lista e multiplique cada número pelo
+        multiplicador e apresente em tela. */
+        
+    }
+}
