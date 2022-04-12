29de76f07d771bdc980518c4e3f320fed11b58bc
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-12 15:14:41 -0300

    teste

--- Teams/d08_04/Exercicio_82.java
+++ Teams/d08_04/Exercicio_82.java
@@ -1,5 +1,7 @@
 package Teams.d08_04;
 
+import java.util.Scanner;
+
 public class Exercicio_82 {
     public static void main(String[] args) {
         /* 82) Faça um programa para corrigir provas de múltipla
@@ -11,6 +13,38 @@ public class Exercicio_82 {
         - Para cada aluno seu número e sua nota;
         - A percentagem de aprovação, sabendo-se que a nota mínima é 6,0 */
 
-        
+        String[] listaGabarito = new String[10];
+        //int[] quantidadeAlunos = new int[15];
+        String[] variavelAlunoGabarito = new String[10];
+        Scanner leitor = new Scanner(System.in);
+        int contadorQuestao = 1;
+        int contadorAluno = 1;
+        int somaNota = 0;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("Informe o gabarito da questão %d (A-E): ", contadorQuestao);
+            listaGabarito[i] = leitor.nextLine();
+            contadorQuestao++;
+            if (contadorQuestao == 11) {
+                contadorQuestao = 0;
+            }
+        }
+
+        System.out.printf("Informe a quantidade de alunos: %n");
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.printf("Informe o gabarito da questão %d, do aluno %d: ", contadorQuestao, contadorAluno);
+                variavelAlunoGabarito[j] = leitor.nextLine();
+                // COMPARAR LISTAS
+                if (variavelAlunoGabarito[j] == listaGabarito[j]) {
+                    somaNota++;
+                }
+                contadorQuestao++;
+            }
+            System.out.printf("A nota do aluno %d é %d%n", contadorAluno, somaNota);
+            contadorAluno++;            
+        }
+
+        leitor.close();
     }
 }
---
+++ Teams/d_12_04/Exercicio_83.java
@@ -0,0 +1,7 @@
+package Teams.d_12_04;
+
+public class Exercicio_83 {
+    public static void main(String[] args) {
+        /* 83) Faça um programa que receba a temperatura média de cada mês do ano e armazene-as em um vetor. Calcule e mostre a maior e a menor temperatura do ano e em que mês elas ocorreram (mostrar o mês por extenso: 1- Janeiro, 2 – Fevereiro). Desconsidere empates. */
+    }
+}
