4ccaa1c36c9a11d3394f0567897b758739fa17ef
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-12 16:14:53 -0300

    teste

--- Teams/d08_04/TesteX.java
+++ Teams/d08_04/TesteX.java
@@ -4,25 +4,11 @@ import java.util.Scanner;
 
 public class TesteX {
     public static void main(String[] args) {
-        int[] listaNumeros = new int[10];
-        int contador = 0;
+        /* 81) Faça um programa que carregue dois vetores, X e Y,
+        com dez números inteiros cada um. Considere que os números
+        de cada vetor digital, X e Y, não podem estar repetidos.
+        Calcule e mostre os seguintes vetores resultantes: */
 
-        Scanner leitor = new Scanner(System.in);
-        for (int i = 0; i < listaNumeros.length; i++) {
-            System.out.printf("Forneça um número: ");
-            if (contador == 0) {
-                listaNumeros[i] = leitor.nextInt();
-                contador++;
-            } else if (contador != 0) {
-                listaNumeros[i] = leitor.nextInt();
-                for (int j = i; j >= 1; j--) {
-                    while (listaNumeros[j] == listaNumeros[i] && j != i) {
-                        System.out.printf("Valor repetido. Informe outro: ");
-                        listaNumeros[i] = leitor.nextInt();
-                    }
-                }
-            }
-        }
-        leitor.close();
+        
     }
 }
