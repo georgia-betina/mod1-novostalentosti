d53f0c2834fc553f0420bde486d13da46eff4d6c
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-13 17:06:41 -0300

    desenvolvimento

--- Teams/d08_04/Exercicio_Nao_Repetir_Resumo.java
+++ Teams/d08_04/Exercicio_Nao_Repetir_Resumo.java
@@ -6,11 +6,32 @@ public class Exercicio_Nao_Repetir_Resumo {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
         int[] x = new int[10];
+        int[] elementosArray = new int[10];
+        int contador = 0;
+
+        for (int i = 0; i < elementosArray.length; i++) {
+            elementosArray[i] = i;
+        }
 
             for (int i = 0; i < x.length; i++) {
-                System.out.printf("Informe um número (X): ");
+                System.out.printf("Informe um número (X) - %d: ", i);
                 x[i] = leitor.nextInt();
-                if (i == 1) {
+                do {
+                    for (int k = 0; k < i; k++) {
+                        if (x[i] == x[k]) {
+                            while (x[i] == x[k]) {
+                                System.out.printf("Número repetido. Informe outro - %d: ", i);
+                                x[i] = leitor.nextInt();
+                            }
+                        }
+                    }
+                    contador++;
+                } while (contador != x.length);
+            }
+        
+        leitor.close();
+
+                /* if (i == 1) {
                     do {
                         if (x[i] == x[0]) {
                             System.out.printf("Número repetido. Informe outro: ");
@@ -182,7 +203,6 @@ public class Exercicio_Nao_Repetir_Resumo {
                         }
                     } while (x[i] == x[0] || x[i] == x[1] || x[i] == x[2] || x[i] == x[3] || x[i] == x[4] || x[i] == x[5] || x[i] == x[6] || x[i] == x[7] || x[i] == x[8]);
                 }
-            }
-            leitor.close();
+            } */
     }
 }
