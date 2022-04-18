ae8a589f30fc8983d953668d094b029cdd1bb115
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-18 16:55:08 -0300

    mudanças

--- Teams/Exercicio_09.java
+++ Teams/Exercicio_09.java
@@ -1,17 +1,26 @@
-package Teams.d17_03;
+package Teams;
 
 import java.util.Scanner;
 
-public class ex2 {
+public class Exercicio_09 {
     public static void main(String[] args) {
+        /*
+        * Exercício 9) Faça um programa que receba o preço de um produto,
+        * calcule e mostre o novo preço, sabendo-se que esse sofreu um
+        * desconto de 10%
+        */
+
+        // ENTRADA
         System.out.println("Informe o valor do produto:");
         Scanner leitor = new Scanner(System.in);
         final float valorinicial = leitor.nextFloat();
         leitor.close();
 
+        // PROCESSAMENTO
         double desconto = valorinicial * 0.1;
         desconto = valorinicial - desconto;
 
+        // SAÍDA
         System.out.println(String.format("O novo valor do produto, com desconto de 10%%, é R$ %.2f.", desconto));
     }
 }
--- Teams/Exercicio_10.java
+++ Teams/Exercicio_10.java
@@ -1,23 +1,30 @@
-package Teams.d17_03;
+package Teams;
 
 import java.util.Scanner;
 
-public class ex3 {
+public class Exercicio_10 {
     public static void main(String[] args) {
+        /*
+        * Exercício 10) Um funcionário recebe um salário fixo mais
+        * 4% de comissão sobre as vendas. Faça um programa que receba
+        * o salário fixo do funcionário e o valor de suas vendas,
+        * calcule e mostre a comissão e seus salário final.
+        */
+
+        // ENTRADAS
         System.out.println("Insira o valor do seu salário:");
         Scanner leitor = new Scanner(System.in);
         final float salario = leitor.nextFloat();
 
         System.out.println("Insira o valor total das suas vendas:");
-        Scanner leitor1 = new Scanner(System.in);
-        final float vendas = leitor1.nextFloat();
+        final float vendas = leitor.nextFloat();
         leitor.close();
-        leitor1.close();
 
+        // PROCESSAMENTO
         double comissao = vendas * 0.04;
-
         double salariofinal = salario + comissao;
 
+        // SAÍDA
         System.out.println(String.format("O salário inicial é de R$ %.2f, o valor das comissões é de R$ %.2f e o salário final é R$ %.2f.", salario, comissao, salariofinal));
     }
 }
