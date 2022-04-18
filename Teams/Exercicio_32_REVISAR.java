2d6a87906729e8e06e62800b730dd2035d1dd8f1
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-18 17:23:14 -0300

    mudanças

--- Teams/Exercicio_32_REVISAR.java
+++ Teams/Exercicio_32_REVISAR.java
@@ -4,29 +4,41 @@ import java.util.Scanner;
 
 public class ex32 {
     public static void main(String[] args) {
+        /*
+        * Exercício 32) Faça um programa que receba três notas de um aluno, calcule
+        * e mostre a média aritmética e a mensagem constante na tabela a seguir.
+        * Aos alunos que ficaram para exame,  calcule e mostre a nota que deverão
+        * tirar para serem aprovados, considerando que a média exigida é 7.0
+        * Média Aritmética      Mensagem
+        * 0,0    ●-------O     3,0      Reprovado
+        * 3,0    ●-------O     7,0   Exame
+        * 7,0    ●-------● 10,00 Aprovado
+        */
+
+        // ENTRADAS
         System.out.println("Digite a primeira nota:");
         Scanner leitor = new Scanner(System.in);
         final float primeiraNota = leitor.nextFloat();
 
         System.out.println("Digite a segunda nota:");
-        Scanner leitor1 = new Scanner(System.in);
-        final float segundaNota = leitor1.nextFloat();
+        final float segundaNota = leitor.nextFloat();
 
         System.out.println("Digite a terceira nota:");
-        Scanner leitor2 = new Scanner(System.in);
-        final float terceiraNota = leitor2.nextFloat();
+        final float terceiraNota = leitor.nextFloat();
         leitor.close();
-        leitor1.close();
-        leitor2.close();
 
+        // PROCESSAMENTO
         final double media = (primeiraNota + segundaNota + terceiraNota) / 3;
 
         if (media >= 7) {
+            // SAÍDA
             System.out.println("Você foi aprovado(a).");
         } else if (media >= 3 & media <= 7) {
             final double mediaExame = 6 - media;
+            // SAÍDA
             System.out.printf("Você está em exame e precisa tirar %.0f ponto(s).", mediaExame);
         } else if (media >= 0 & media <= 3) {
+            // SAÍDA
             System.out.println("Você está reprovado(a).");
         }
     }
