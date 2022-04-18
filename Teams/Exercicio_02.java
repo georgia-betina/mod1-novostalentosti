7f58867e66454ca7de115a86efdaa327ddf5c441
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-18 16:35:44 -0300

    mudanças

--- Teams/Exercicio_02.java
+++ Teams/Exercicio_02.java
@@ -1,10 +1,13 @@
-package Teams.d16_03;
+package Teams;
 
 import java.util.Scanner;
 
-public class ex2_16 {
+public class Exercicio_02 {
     public static void main(String[] args) {
-        // Exercício 2) Faça um programa para calcular a quantidade de carne necessária para um churrasco de acordo com o número de pessoas convidadas. Considere que todas são adultas e que um adulto consome 250 gramas de carne por refeição.
+        /* Exercício 2) Faça um programa para calcular a quantidade
+        * de carne necessária para um churrasco de acordo com o número
+        * de pessoas convidadas. Considere que todas são adultas e que
+        * um adulto consome 250 gramas de carne por refeição. */
 
         // ENTRADA
         System.out.println("Olá, querido usuário. Por favor, indique a quantidade de pessoas que estão convidadas para o churrasco:");
@@ -17,8 +20,10 @@ public class ex2_16 {
 
         if(qtdecarne >= 1000){
             final float qtdecarnef = qtdecarne / 1000;
+            // SAÍDA
             System.out.println(String.format("A quantidade necessária de carne para o churrasco é %.1fkg.", qtdecarnef));
         } else {
+            // SAÍDA
             System.out.println(String.format("A quantidade necessária de carne é %dg.", qtdecarne));
         }
     }
