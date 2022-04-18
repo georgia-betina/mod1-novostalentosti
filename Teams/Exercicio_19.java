8d1354251673c5cd477d6e67d85877c2fb53862d
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-18 17:07:01 -0300

    mudanças

---
+++ Teams/Exercicio_19.java
@@ -0,0 +1,44 @@
+package Teams;
+
+import java.util.Scanner;
+
+public class ex12 {
+    public static void main(String[] args) {
+        /*
+        * Exercício 19) Sabe-se que, para iluminar de maneira correta os
+        * cômodos de uma casa, para cada metro quadrado, deve-se usar
+        * 18Watts de potência. Faça um programa que receba as duas
+        * dimensões de um cômodo (em metros), calcule e  mostre a sua
+        * área (em metros quadrados) e a potência de iluminação que
+        * deverá ser utilizada.
+        */
+
+        // ENTRADAS
+        System.out.println("Informe a potência da lâmpada utilizada:");
+        Scanner leitor = new Scanner(System.in);
+        final float potencia = leitor.nextFloat();
+    
+        System.out.println("Informe a largura do cômodo em metros:");
+        final float largura = leitor.nextFloat();
+    
+        System.out.println("Informe o comprimento do cômodo em metros:");
+        final float comprimento = leitor.nextFloat();
+    
+        leitor.close();
+    
+        // PROCESSAMENTO
+        final double metrosquadrados = largura * comprimento;
+    
+        final int lampExistentes = (int)potencia / 18;
+    
+        double lampNecessarias = metrosquadrados / 18;
+        lampNecessarias = (int)Math.ceil(lampNecessarias);
+    
+        final double potenciaNecessaria = metrosquadrados * 18;
+    
+        String resultado = lampExistentes>=lampNecessarias?"suficiente":"insuficiente";
+    
+        // SAÍDA
+        System.out.println(String.format("Cômodo em m²: %.0f.%nA quantidade de potência necessária para iluminar o cômodo será de %.0fW.%nA quantidade de lâmpadas existentes é %d e a quantidade necessária é %.0f. Ou seja, a quantidade existente é %s.", metrosquadrados, potenciaNecessaria, lampExistentes, lampNecessarias, resultado));
+    }
+}
--- Teams/Exercicio_20.java
+++ Teams/Exercicio_20.java
@@ -1,26 +1,37 @@
-package Teams.d17_03;
+package Teams;
 
 import java.util.Scanner;
 
 public class ex13 {
     public static void main(String[] args) {
+        /*
+        * Exercício 20) Faça um programa que receba uma hora (uma
+        * variável para hora e outra para minutos), calcule e mostre:
+        *  - a hora digitada convertida em minutos;
+        *  - o total dos minutos, ou seja, os minutos digitados mais
+        * a conversão anterior;
+        *  - o total dos minutos convertidos em segundos.
+        */
+
+        // ENTRADAS
         System.out.println("Digite apenas as horas (sem os minutos):");
         Scanner leitor = new Scanner(System.in);
         final int horas = leitor.nextInt();
 
         System.out.println("Digite apenas os minutos:");
         Scanner leitor1 = new Scanner(System.in);
-        final int minutos = leitor1.nextInt();
+        final int minutos = leitor.nextInt();
 
         leitor.close();
-        leitor1.close();
 
+        // PROCESSAMENTO
         final int horasMinutos = horas * 60;
 
         final int totalMinutos = horasMinutos + minutos;
 
         final int totalSegundos = totalMinutos * 60;
 
+        // SAÍDA
         System.out.println(String.format("As horas convertidas em minutos representam %d minutos.%nO total de minutos é %d.%nO total de minutos convertido em segundos é %d.", horasMinutos, totalMinutos, totalSegundos));
     }
 }
