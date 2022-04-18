5ab96de3283d22a6236486ca27292a5aa9228019
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-18 16:08:58 -0300

    aula 18/04

---
+++ intro_POO/App.java
@@ -0,0 +1,33 @@
+package intro_POO;
+
+import intro_POO.Pessoa;
+
+public class App {
+    public static void main(String[] args) {
+
+    // Criando uma variável do tipo Pessoa
+    // Criando uma instância de objeto a partir da classe Pessoa
+        
+    Pessoa zezinho = new Pessoa();
+    zezinho.nome = "Zezinho da Silva Sauro";
+    zezinho.altura = 1.75f;
+
+    Pessoa luizinho = new Pessoa();
+    luizinho.nome = "Luizinho da Silva Sauro Jr";
+    luizinho.altura = 1.68f;
+
+
+    System.out.println(zezinho.nome);
+    System.out.println(luizinho.nome);
+
+    System.out.println(zezinho.altura);
+    System.out.println(luizinho.altura);
+
+    zezinho.altura = 0f;
+    luizinho.altura = 0f;
+
+    System.out.println(zezinho.altura);
+    System.out.println(luizinho.altura);
+
+    }
+}
---
+++ intro_POO/Pessoa.java
@@ -0,0 +1,10 @@
+package intro_POO;
+
+public class Pessoa {
+    // Atributos
+    public String nome;
+    public int idade;
+    public String CPF;
+    public float altura;
+
+}
