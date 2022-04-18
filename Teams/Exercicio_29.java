9d88c9b46254e966c7a93a603709b821d4be82b4
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-18 17:21:45 -0300

    mudanças

--- Teams/Exercicio_29.java
+++ Teams/Exercicio_29.java
@@ -4,14 +4,23 @@ import java.util.Scanner;
 
 public class ex29 {
     public static void main(String[] args) {
+        /*
+        * Exercício 29) Faça um programa que receba um número inteiro
+        * e mostre se ele é par ou impar.
+        */
+
+        // ENTRADA
         System.out.println("Digite um número");
         Scanner leitor = new Scanner(System.in);
         final float numero = leitor.nextFloat();
         leitor.close();
 
+        // PROCESSAMENTO
         if (numero%2 == 0) {
+            // SAÍDA
             System.out.println("Seu número é par!");
         } else {
+            // SAÍDA
             System.out.println("Seu número é ímpar!");
         }
     }
--- Teams/Exercicio_30.java
+++ Teams/Exercicio_30.java
@@ -2,19 +2,30 @@ package teams;
 
 import java.util.Scanner;
 
-public class ex30 {
+public class Exercicio_30 {
     public static void main(String[] args) {
+        /*
+        * Exercício 30) Uma empresa decide dar um aumento de 30% aos funcionários
+        * com salários inferiores a R$ 500,00. Faça um programa que receba o salário
+        * do funcionário e mostre o valor do salário reajustado ou uma mensagem caso
+        * ele não tenha direito ao aumento. 
+        */
+
+        // ENTRADA
         System.out.println("Informe o valor do seu salário:");
         Scanner leitor = new Scanner(System.in);
         final float salario = leitor.nextFloat();
         leitor.close();
 
+        // PROCESSAMENTO
         if (salario < 500) {
             final float aumento = 0.3f;
             double novoSalario = salario * aumento;
             novoSalario = salario + novoSalario;
+            // SAÍDA
             System.out.println(String.format("Seu novo salário será de R$%.2f", novoSalario));
         } else {
+            // SAÍDA
             System.out.println("Você não tem direito ao aumento.");
         }
     }
