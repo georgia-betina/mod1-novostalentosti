660964f2ec91b29b65fa60f052160efff2d6de40
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-18 17:04:17 -0300

    mudanças

--- Teams/ex11.java
+++ Teams/ex11.java
@@ -1,17 +1,25 @@
-package Teams.d17_03;
+package Teams;
 
 import java.util.Scanner;
 
 public class ex11 {
     public static void main(String[] args) {
-        // import
+        /*
+        * Exercício 18) Faça um programa que receba uma temperatura em
+        * Celsius, calcule e mostre essa temperatura em Fahrenheit.
+        * Sabe-se que F = C x 1,8 + 32
+        */
+
+        // ENTRADA
         System.out.println("Coloque a temperatura em graus Celsius:");
         Scanner leitor = new Scanner(System.in);
         final int temperaturaC = leitor.nextInt();
         leitor.close();
 
+        // PROCESSAMENTO
         final float conversao = ((float)temperaturaC + 32) / 100 * 180;
 
+        // SAÍDA
         System.out.println(String.format("A conversão da temperatura para Fahrenheit é %.1f °F.", conversao));
     }
 }
