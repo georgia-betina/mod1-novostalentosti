378875560a2684aa834b59cf44de66985b41f665
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-18 16:47:39 -0300

    mudanças

--- Teams/Exercicio_08.java
+++ Teams/Exercicio_08.java
@@ -2,7 +2,7 @@ package Teams;
 
 import java.util.Scanner;
 
-public class Exercicio_01_treino {
+public class Exercicio_08 {
     public static void main(String[] args) {
         /*
         * Exercício 08) Faça um programa que receba um número real, encontre
@@ -13,17 +13,19 @@ public class Exercicio_01_treino {
         * subtrair a parte inteira);
         * - o arredondamento desse número; 3 (fazer o arredondamento).
         */
+
+        // ENTRADA
         System.out.println("Acrescente um numero real:");
         Scanner leitor = new Scanner(System.in);
         float numeroreal = leitor.nextFloat();
         leitor.close();
 
+        // PROCESSAMENTO
         int numerointeiro = (int)numeroreal;
-
         float parteinteira = numeroreal - numerointeiro;
-
         float arredondamento = Math.round(numeroreal);
 
+        // SAÍDA
         System.out.println(String.format("O número inteiro é: %d%nA parte fracionária é: %.2f%nO arredondamento é: %.0f", numerointeiro, parteinteira, arredondamento));
     }
 }
--- Teams/Exercicio_34.java
+++ Teams/Exercicio_34.java
@@ -1,4 +1,4 @@
-package Teams.d23_03;
+package Teams;
 
 import java.util.Scanner;
 
