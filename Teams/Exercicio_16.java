534ac114b1566dee51acebb9f5c68921e596672e
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-18 17:03:33 -0300

    mudanças

---
+++ Teams/Exercicio_16.java
@@ -0,0 +1,33 @@
+package Teams;
+
+import java.util.Scanner;
+
+import javax.net.ssl.SSLPeerUnverifiedException;
+import javax.sound.sampled.SourceDataLine;
+
+public class ex9 {
+    public static void main(String[] args) {
+        /*
+        * Exercício 16) Faça um programa que receba o valor do
+        * salario mínimo e o valor do salário de um funcionário,
+        * calcule e mostre a quantidade de salários mínimos que
+        * esse funcionário ganha.
+        */
+
+        // ENTRADAS
+        System.out.println("Informe o valor do seu salário:");
+        Scanner leitor = new Scanner(System.in);
+        final float salario = leitor.nextFloat();
+
+        System.out.printf("Informe o valor do salário mínimo: R$ ");
+        final float salariominimo = leitor.nextFloat();
+
+        leitor.close();
+
+        // PROCESSAMENTO
+        final float diferencadesalarios = salario / salariominimo;
+
+        // SAÍDA
+        System.out.println(String.format("Você ganha %.2fx ", diferencadesalarios) + (diferencadesalarios>=0?"a mais":"a menos") + (" em relação ao salário mínimo de 2022."));
+    }
+}
--- Teams/Exercicio_17.java
+++ Teams/Exercicio_17.java
@@ -1,25 +1,30 @@
-package Teams.d17_03;
+package Teams;
 
 import java.util.Scanner;
 
 public class ex10 {
     public static void main(String[] args) {
+        /*
+        * Exercício 17) João recebeu seu salário e precisa pagar duas
+        * contas atrasadas. Em razão do seu atraso, ele deverá pagar
+        * multa de 2% sobre cada conta. Faça um programa que calcule
+        * e mostre quanto restará do salário de João.
+        */
+
+        // ENTRADAS 
         System.out.println("Informe o valor do seu salário:");
         Scanner leitor = new Scanner(System.in);
         final float salario = leitor.nextFloat();
 
         System.out.println("Informe o valor da primeira conta atrasada:");
-        Scanner leitor1 = new Scanner(System.in);
-        final float conta1 = leitor1.nextFloat();
+        final float conta1 = leitor.nextFloat();
 
         System.out.println("Informe o valor da segunda conta atrasada:");
-        Scanner leitor2 = new Scanner(System.in);
-        final float conta2 = leitor2.nextFloat();
+        final float conta2 = leitor.nextFloat();
 
         leitor.close();
-        leitor1.close();
-        leitor2.close();
 
+        // PROCESSAMENTO
         final float multa = 0.02f;
         double atrasoconta1 = conta1 * multa;
         atrasoconta1 = conta1 + atrasoconta1;
@@ -29,6 +34,7 @@ public class ex10 {
 
         final double resto = salario - atrasoconta1 - atrasoconta2;
 
+        // SAÍDA
         System.out.println(String.format("O resto será R$ %.2f.", resto));
     }
 }
