94f15480bccdc70dd5d81dcd48ef6c5ba7d00b9e
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-18 17:25:11 -0300

    mudanças

--- Teams/Exercicio_33.java
+++ Teams/Exercicio_33.java
@@ -4,26 +4,35 @@ import java.util.Scanner;
 
 public class ex33 {
     public static void main(String[] args) {
+        /*
+        * Exercício 33) Faça um programa que receba:
+        * - o código de um produto comprado, supondo que a digitação
+        * do código do produto seja sempre válida, isso é, um número
+        * inteiro entre 1 e 10;
+        * - o peso do produto em quilos;
+        * - o código do país de origem, supondo que a digitação do
+        * código seja sempre válida, isso é, um número inteiro entre
+        * 1 e 3.
+        */
+
+        // ENTRADAS
         System.out.println("Digite o código do produto:");
         Scanner leitor = new Scanner(System.in);
         final int codigoProduto = leitor.nextInt();
 
         System.out.println("Digite o peso do produto (em kg):");
-        Scanner leitor1 = new Scanner(System.in);
-        final float peso = leitor1.nextFloat();
+        final float peso = leitor.nextFloat();
 
         System.out.println("Digite o código do país de origem:");
-        Scanner leitor2 = new Scanner(System.in);
-        final int codigoPais = leitor2.nextInt();
+        final int codigoPais = leitor.nextInt();
 
         final int gramas = 1000;
         final double pesoGramas = peso * (float)gramas;
         System.out.printf("O peso do seu produto em gramas é %.0fg%n", pesoGramas);
 
         leitor.close();
-        leitor1.close();
-        leitor2.close();
 
+        // PROCESSAMENTO
         double precoGramaFinal1 = 0, precoGramaFinal2 = 0, precoGramaFinal3 = 0;
         double novoPreco1 = 0, novoPreco2 = 0, novoPreco3 = 0, novoPreco = 0, novoPrecoTotal = 0;
         double valorcImposto1 = 0, valorcImposto2 = 0;
@@ -35,6 +44,7 @@ public class ex33 {
             case 4:
                 final int precopGrama1 = 10;
                 precoGramaFinal1 = pesoGramas * (float)precopGrama1;
+                // SAÍDA
                 System.out.printf("O preço do produto é R$%.2f%n", precoGramaFinal1);
                 novoPreco1 = precoGramaFinal1;
                 break;
@@ -43,6 +53,7 @@ public class ex33 {
             case 7:
                 final int precopGrama2 = 25;
                 precoGramaFinal2 = pesoGramas * precopGrama2;
+                // SAÍDA
                 System.out.printf("O preço do produto é R$%.2f%n", precoGramaFinal2);
                 novoPreco2 = precoGramaFinal2;
                 break;
@@ -51,6 +62,7 @@ public class ex33 {
             case 10:
                 final int precopGrama3 = 35;
                 precoGramaFinal3 = pesoGramas * precopGrama3;
+                // SAÍDA
                 System.out.printf("O preço do produto é R$%.2f%n", precoGramaFinal3);
                 novoPreco3 = precoGramaFinal3;
                 break;
@@ -68,12 +80,14 @@ public class ex33 {
 
         switch (codigoPais) {
             case 1:
+                // SAÍDA
                 System.out.println("O valor do imposto é R$0,00");
                 novoPrecoTotal = novoPreco;
                 break;
             case 2:
                 final float imposto1 = 0.15f;
                 valorcImposto1 = novoPreco * imposto1;
+                // SAÍDA
                 System.out.printf("O valor do imposto é R$%.2f%n", valorcImposto1);
                 valorcImposto1 = novoPreco + valorcImposto1;
                 novoPrecoTotal = valorcImposto1;
@@ -81,12 +95,14 @@ public class ex33 {
             case 3:
                 final float imposto2 = 0.25f;
                 valorcImposto2 = novoPreco * imposto2;
+                // SAÍDA
                 System.out.printf("O valor do imposto é R$%.2f%n", valorcImposto2);
                 valorcImposto2 = novoPreco + valorcImposto2;
                 novoPrecoTotal = valorcImposto2;
                 break;
         }
 
+        // SAÍDA
         System.out.printf("O valor do produto é R$ %.2f e o preço total é R$ %.2f.", novoPreco, novoPrecoTotal);
     }
 }
