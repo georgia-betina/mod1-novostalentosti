7e27a96c3488ae0e527fd6fa90d3a05b31a1fcd9
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-18 16:56:06 -0300

    mudanças

--- Teams/ex4.java
+++ Teams/ex4.java
@@ -1,20 +1,30 @@
-package Teams.d17_03;
+package Teams;
 
 import java.util.Scanner;
 
 public class ex4 {
     public static void main(String[] args) {
+        /*
+        * Exercício 11) Faça um programa que receba o peso de uma pessoa,
+        * calcule e mostre:
+        * - o novo peso, se a pessoa engordar 15% sobre o peso digitado;
+        * - o novo peso, se a pessoa emagrecer 20% sobre o peso digitado.
+        */
+
+        // ENTRADAS
         System.out.println("Por favor, insira o valor do seu peso em kg:");
         Scanner leitor = new Scanner(System.in);
         final float peso = leitor.nextFloat();
         leitor.close();
 
+        // PROCESSAMENTO
         double aumento15 = peso * 0.15;
         aumento15 = peso + aumento15;
 
         double emagrece20 = peso * 0.2;
         emagrece20 = peso - emagrece20;
 
+        // SAÍDA
         System.out.println(String.format("Se você engordar 15%% do peso atual, ficará com %.1fkg. Se emagrecer 20%%, ficará com %.1fkg.", aumento15, emagrece20));
     }
 }
