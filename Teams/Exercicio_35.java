3f7b0e9cc6e0c103eae042fc084939d39c41dc17
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-19 16:40:00 -0300

    ex 35

--- Teams/Exercicio_35.java
+++ Teams/Exercicio_35.java
@@ -4,6 +4,18 @@ import java.util.Scanner;
 
 public class Exercicio_35 {
     public static void main(String[] args) {
+        /*
+        * Exercício 35) O custo ao consumidor de um carro novo é a soma
+        * do preço da fábrica com o percentual de lucro do distribuidor
+        * e dos impostos aplicados ao preço de fábrica. Faça um programa
+        * que receba o preço de fábrica de um veículo, o percentual de
+        * lucro do distribuidor e o percentual de impostos. Calcule e
+        * mostre:
+        * a. O valor correspondente ao lucro do distribuidor
+        * b. O valor correspondente aos impostos
+        * c. O preço final do veículo
+        */
+        
         System.out.printf("Informe o valor de fábrica do automóvel: R$ ");
         Scanner leitor = new Scanner(System.in);
         final float valorFabrica = leitor.nextFloat();
