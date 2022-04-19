888c494fcff9e0fabd08df8477541dec7efedee5
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-19 16:41:54 -0300

    ex 37

--- Teams/Exercicio_37.java
+++ Teams/Exercicio_37.java
@@ -4,6 +4,20 @@ import java.util.Scanner;
 
 public class Exercicio_37 {
     public static void main(String[] args) {
+        /*
+        * Exercício 37) Faça um programa que receba o código correspondente
+        * ao cargo de um funcionário e seu salário atual e mostre o cargo,
+        * o valor do aumento e seu novo salário. Os cargos estão na seguinte
+        * tabela:
+        *
+        * Código    Cargo            Percentual
+        * 1         Escriturário     50%
+        * 2         Secretário       35%
+        * 3         Caixa            20%
+        * 4         Gerente          10%
+        * 5         Diretor          Não tem aumento
+        */
+
         System.out.printf("Informe o valor do seu salário: R$ ");
         Scanner leitor = new Scanner(System.in);
         final float salarioBase = leitor.nextFloat();
