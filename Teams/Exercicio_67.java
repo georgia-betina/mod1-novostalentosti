fed78944f2d3e11ad0638d80b25f65c41eba6fb9
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-19 17:01:20 -0300

    ex 67

--- Teams/Exercicio_67.java
+++ Teams/Exercicio_67.java
@@ -4,16 +4,17 @@ import java.util.Scanner;
 
 public class Exercicio_67 {
     public static void main(String[] args) {
-        /* 67) Faça um programa que apresente os resultados
-        de uma tabuada de um número qualquer, a qual deve ser
-        impressa no seguinte formato:
-        Considerando como exemplo o fornecimento do número 2
-        para o número qualquer, ter-se-ia a seguinte situação:
-        2 x 1 = 2
-        2 x 2 = 4
-        2 x 3 = 6
-        ...
-        2 x 10 = 20 */
+        /*
+        * Exercício 67) Faça um programa que apresente os resultados de uma tabuada
+        * de um número qualquer, a qual deve ser impressa no seguinte formato:
+        * Considerando como exemplo o fornecimento do número 2 para o número qualquer,
+        * ter-se-ia a seguinte situação:
+        * 2 x 1 = 2
+        * 2 x 2 = 4
+        * 2 x 3 = 6
+        * ...
+        * 2 x 10 = 20
+        */
 
         System.out.printf("Informe um número inteiro e positivo para calcular a sua tabuada: ");
         Scanner leitor = new Scanner(System.in);
