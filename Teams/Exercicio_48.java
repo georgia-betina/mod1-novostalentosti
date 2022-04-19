eb36e365b7fed68bcd9dcdb21f9010baf3da12a6
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-19 16:51:57 -0300

    ex 48

--- Teams/Exercicio_48.java
+++ Teams/Exercicio_48.java
@@ -4,13 +4,15 @@ import java.util.Scanner;
 
 public class Exercicio_48 {
     public static void main(String[] args) {
-        /* Escreva o algoritmo em FLUXOGRAMA que calcule o valor total de
-        aluguel e taxas a serem cobrados de um morador. Solicite o valor do
-        aluguel, o valor do condomínio, considere que o valor do metro cubico
-        do gas é de R$ 1,80 e o valor da taxa de consumo de agua é de R$ 0,60
-        por metro cubico de agua consumida. Apresente também o valor de 5% do
-        valor total do aluguel que pode ser dado como desconto caso seja pago
-        5 dias antes do vencimento. */
+        /*
+        * Exercício 48) Escreva um programa que calcule o valor total de aluguel
+        * e taxas a serem cobrados de um morador. Solicite o valor do aluguel, o
+        * valor do condomínio, considere que o valor do metro cubico do gas é de
+        * R$ 1,80 e o valor da taxa de consumo de agua é de R$ 0,60 por metro
+        * cúbico de agua consumida. Apresente também o valor de 5% do valor total
+        * do aluguel que pode ser dado como desconto caso seja pago 5 dias antes
+        * do vencimento.
+        */
 
         System.out.printf("Insira o valor do aluguel: R$ ");
         Scanner leitor = new Scanner(System.in);
