29d43bb5c671a2160625d88681f1fa0dd280ea27
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-19 14:51:12 -0300

    aula

--- intro_POO/App.java
+++ intro_POO/App.java
@@ -8,14 +8,17 @@ public class App {
     // Criando uma variável do tipo Pessoa
     // Criando uma instância de objeto a partir da classe Pessoa
         
+    // passagem de parâmetros
+    // instanciação
     Pessoa zezinho = new Pessoa("Zezinho");
     //zezinho.nome = "Zezinho da Silva Sauro";
     zezinho.altura = 1.75f;
+    zezinho.idade = 22;
 
     Pessoa luizinho = new Pessoa("Luiz");
     //luizinho.nome = "Luizinho da Silva Sauro Jr";
     luizinho.altura = 1.68f;
-
+    luizinho.idade = 17;
 
     System.out.println(zezinho.nome);
     System.out.println(luizinho.nome);
@@ -29,5 +32,9 @@ public class App {
     System.out.println(zezinho.altura);
     System.out.println(luizinho.altura);
 
+    String guardaResultado = zezinho.podeDirigir();
+    System.out.println(guardaResultado);
+    guardaResultado = luizinho.podeDirigir();
+    System.out.println(guardaResultado);
     }
 }
--- intro_POO/Pessoa.java
+++ intro_POO/Pessoa.java
@@ -8,10 +8,25 @@ public class Pessoa {
     public String CPF;
     public float altura;
 
-    // comportamento
+    // Comportamentos
     // CONSTRUTOR - inicializar um objeto e definir valores obrigatórios
     public Pessoa(String nome){ // escopo de vida local
         System.out.println("CONTRUTOR:" + nome);
         this.nome = nome;
+    }
+
+    // método (em outras linguagens mais antigas é chamado de função)
+    // assinatura de um método
+    // modificadorDeAcesso tipoDeRetorno nomeDoMetodo(parametros) { corpo }
+    public String podeDirigir(){
+        if(this.idade >= 18){ // não precisava utilizar o this porque não há outra variável no mesmo bloco com o mesmo nome
+            return "Pode dirigir";
+        } else {
+            return "Não pode dirigir";
+        }
+    }
+
+    public void mostraCPF(){
+        System.out.println(this.CPF);
     }
 }
