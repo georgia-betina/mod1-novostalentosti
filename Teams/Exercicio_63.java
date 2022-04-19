104bfcc75e357de8cfb63c87518fd3aabfa7c464
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-19 16:54:51 -0300

    ex 63

--- Teams/Exercicio_63.java
+++ Teams/Exercicio_63.java
@@ -4,15 +4,18 @@ import java.util.Scanner;
 
 public class Exercicio_63 {
     public static void main(String[] args) {
-        /* 63) Faça um programa que receba vários números, calcule e mostre:
-        - a soma dos números digitados;
-        - a quantidade de números digitados;
-        - a média dos números digitados;
-        - o maior número digitado;
-        - o menor número digitado;
-        - a média dos números pares;
-        - a porcentagem dos números ímpares entre todos os números digitados.
-        Finalize a entrada de dados com a digitação do número 30000. */
+        /*
+        * Exercício 63) Faça um programa que receba vários números, calcule
+        * e mostre:
+        * - a soma dos números digitados;
+        * - a quantidade de números digitados;
+        * - a média dos números digitados;
+        * - o maior número digitado;
+        * - o menor número digitado;
+        * - a média dos números pares;
+        * - a porcentagem dos números ímpares entre todos os números digitados.
+        * Finalize a entrada de dados com a digitação do número 30000.
+        */
 
         Scanner leitor = new Scanner(System.in);
         float entradaNumero = 0;
