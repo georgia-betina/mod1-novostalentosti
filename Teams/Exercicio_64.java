c9403274dda63934dd1708cb712bf8b2eaacafb8
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-19 16:56:32 -0300

    ex 64

--- Teams/Exercicio_64.java
+++ Teams/Exercicio_64.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class Exercicio_64 {
     public static void main(String[] args) {
-        /* 64) Faça um algoritmo que calcule a multiplicação
-        de dois números inteiros sem utilizar o operador “*”.
-        Em vez disso, utilize apenas o operador de adição “+”.
-        Para implementar esse algoritmo, devemos lembrar que
-        qualquer multiplicação pode ser expressa por meio de
-        somas. Por exemplo, o resultado da expressão 6 * 3 é
-        o mesmo que 6 + 6 + 6 ou 3 + 3 + 3 + 3 + 3 + 3. Ou
-        seja, soma-se um elemento com ele próprio o número
-        de vezes do segundo elemento. */
+        /*
+        * Exercício 64) Faça um algoritmo que calcule a multiplicação de dois
+        * números inteiros sem utilizar o operador “*”. Em vez disso, utilize
+        * apenas o operador de adição “+”. Para implementar esse algoritmo,
+        * devemos lembrar que qualquer multiplicação pode ser expressa por meio
+        * de somas. Por exemplo, o resultado da expressão 6 * 3 é o mesmo que 6
+        * + 6 + 6 ou 3 + 3 + 3 + 3 + 3 + 3. Ou seja, soma-se um elemento com ele
+        * próprio o número de vezes do segundo elemento.
+        */
 
         Scanner leitor = new Scanner(System.in);
         System.out.printf("Digite um número: ");
