12c085cd10cf540fa84c658f7cb346dd5578128c
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-19 16:38:53 -0300

    ex 34

--- Teams/Exercicio_34.java
+++ Teams/Exercicio_34.java
@@ -4,6 +4,13 @@ import java.util.Scanner;
 
 public class Exercicio_34 {
     public static void main(String[] args) {
+        /*
+        * Exercício 34) Faça um programa que receba o salário-base de um
+        * funcionário, calcule e mostre o salário a receber, sabendo-se
+        * que esse funcionário tem gratificação de 5% sobre o salário-
+        * base e paga imposto de 7% sobre o salário-base.
+        */
+
         System.out.printf("Informe o valor do seu salário: R$ ");
         Scanner leitor = new Scanner(System.in);
         final float salarioBase = leitor.nextFloat();
