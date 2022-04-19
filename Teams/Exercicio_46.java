3c7a4f253fbf2085222005ef29aae918cfc5e5bc
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-19 16:50:19 -0300

    ex 46

--- Teams/Exercicio_46.java
+++ Teams/Exercicio_46.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 
 public class Exercicio_46 {
     public static void main(String[] args) {
-        /* Construa um algoritmo que calcule o consumo de combustível
-        de um veículo qualquer baseado nas informações de quilometragem 
-        inicial e final de um percurso, e a quantidade de gasolina consumida
-        em litros. Caso o consumo esteja entre 10 e 16 litros/km mostre a
-        mensagem “consumo normal”, caso seja maior que 16 litros/km apresente
-        a mensagem “consumo anormal”. */
+        /*
+        * Exercício 46) Construa um algoritmo que calcule o consumo de combustível
+        * de um veículo qualquer baseado nas informações de quilometragem  inicial
+        * e final de um percurso, e a quantidade de gasolina consumida em litros.
+        * Caso o consumo esteja entre 10 e 16 litros/km mostre a mensagem “consumo
+        * normal”, caso seja maior que 16 litros/km apresente a mensagem “consumo
+        * anormal”.
+        */
 
         System.out.printf("Insira o valor da quilometragem inicial: ");
         Scanner leitor = new Scanner(System.in);
