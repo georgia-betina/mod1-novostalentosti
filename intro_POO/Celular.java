91ca070c52cac00f8125b022349d9a31dacc6b9c
georgia betina <georgia_betina@hotmail.com>
2022-04-20 14:44:28 +0100

    aula

---
+++ intro_POO/Celular.java
@@ -0,0 +1,31 @@
+package intro_POO;
+
+public class Celular {
+    private String numero;
+    private String marca;
+    private String modelo;
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+}
