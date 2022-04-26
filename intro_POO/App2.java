a78c66a9a3a607f42ec26e5dd596ebc54ee4ccb9
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-26 14:00:22 -0300

    correção módulos

--- intro_POO/App2.java
+++
@@ -1,9 +0,0 @@
-package intro_POO;
-
-import intro_POO.introducao.bichos.Animal;
-
-public class App2 {
-    public static void main(String[] args) {
-        Animal animal = new Animal();
-    }
-}
